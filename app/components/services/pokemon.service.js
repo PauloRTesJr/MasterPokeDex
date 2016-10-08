6b77c0b18f3c56d4e8b5017d5962bed602d21125
PauloRTesJr <paulortesjr@gmail.com>
2016-10-08 17:48:51 -0300

    Icones pokemon, paginação pokedex.

--- app/components/services/pokemon.service.js
+++ app/components/services/pokemon.service.js
@@ -3,6 +3,7 @@
 angular.module('apiApp')
     .factory('Pokemons', function($resource) {
         return $resource('http://pokeapi.co/api/v2/pokemon/:id',{id: '@id'}, {
-            query: {method: 'get', isArray: false}
+            query: {method: 'get', isArray: false},
+            refresh: {method: 'get', params:{offset:true}, isArray: false}
         });
     });
--- app/pokedex/pokedex.controller.js
+++ app/pokedex/pokedex.controller.js
@@ -1,13 +1,52 @@
 'use strict';
 
 angular.module('apiApp')
-    .controller('PokedexCtrl', function ($scope, Pokemons) {
-    	$scope.pokemons = [];
-    	$scope.next = '';
+    .controller('PokedexCtrl', function($scope, Pokemons, $mdDialog, $http) {
+        $scope.pokemons = [];
+        $scope.offset = 0;
 
-    	$scope.query = Pokemons.query(function(data) {
-		     $scope.pokemons = data.results;
-		     $scope.next = data.next;
-		});
-    	console.log($scope.pokemons);
+        $scope.query = Pokemons.query(function(data) {
+            $scope.pokemons = data.results;
+            $scope.next = data.next;
+            $scope.previous = data.previous;
+        });
+        console.log($scope.pokemons);
+
+        $scope.showPokemon = function(ev, pokemon) {
+        	$scope.querying = true;
+        	$mdDialog.show({
+                contentElement: '#myStaticDialog',
+                targetEvent: ev,
+                clickOutsideToClose: true
+            });
+            console.log(pokemon);
+            $http({
+                method: 'GET',
+                url: pokemon.url
+            }).then(function successCallback(response) {
+            	$scope.pokemon = response.data;
+            	$scope.querying = false;
+            }, function errorCallback(response) {
+                alert('Falha ao carregar o conteúdo!');
+            });
+
+        };
+
+        $scope.refreshPokemon = function(direction){
+            if(direction == 'next'){
+                $scope.offset += 20;
+            }
+            if(direction == 'previous'){
+                $scope.offset -= 20;
+            }
+            $scope.pokemons =[];
+            $scope.next = '';
+            $scope.previous = '';
+            $scope.query = Pokemons.refresh({offset: $scope.offset}, function(data) {
+                $scope.pokemons = data.results;
+                $scope.next = data.next;
+                $scope.previous = data.previous;
+            });
+        }
+
     });
