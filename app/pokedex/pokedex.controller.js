da3eb855ff5145e88a9b8377f02e572896906814
PauloRTesJr <paulortesjr@gmail.com>
2016-10-13 23:03:40 -0300

    more stuff modal and buttons

--- app/pokedex/pokedex.controller.js
+++ app/pokedex/pokedex.controller.js
@@ -13,8 +13,8 @@ angular.module('apiApp')
         console.log($scope.pokemons);
 
         $scope.showPokemon = function(ev, pokemon) {
-        	$scope.querying = true;
-        	$mdDialog.show({
+            $scope.querying = true;
+            $mdDialog.show({
                 contentElement: '#myStaticDialog',
                 targetEvent: ev,
                 clickOutsideToClose: true
@@ -24,29 +24,45 @@ angular.module('apiApp')
                 method: 'GET',
                 url: pokemon.url
             }).then(function successCallback(response) {
-            	$scope.pokemon = response.data;
-            	$scope.querying = false;
+                $scope.pokemon = response.data;
+                angular.forEach($scope.pokemon.types, function(value, key) {
+                    $http({
+                        method: 'GET',
+                        url: value.type.url
+                    }).then(function successCallback(response) {
+                        value.infos = response.data;
+                    });
+                });
+                $scope.querying = false;
             }, function errorCallback(response) {
                 alert('Falha ao carregar o conteúdo!');
             });
 
         };
 
-        $scope.refreshPokemon = function(direction){
-            if(direction == 'next'){
+        $scope.refreshPokemon = function(direction) {
+            if (direction == 'next') {
                 $scope.offset += 20;
             }
-            if(direction == 'previous'){
+            if (direction == 'previous') {
                 $scope.offset -= 20;
             }
-            $scope.pokemons =[];
+            $scope.pokemons = [];
             $scope.next = '';
             $scope.previous = '';
-            $scope.query = Pokemons.refresh({offset: $scope.offset}, function(data) {
+            $scope.query = Pokemons.refresh({ offset: $scope.offset }, function(data) {
                 $scope.pokemons = data.results;
                 $scope.next = data.next;
                 $scope.previous = data.previous;
             });
         }
+
+        $scope.hoverIn = function() {
+            this.hoverEdit = true;
+        };
+
+        $scope.hoverOut = function() {
+            this.hoverEdit = false;
+        };
 
     });
